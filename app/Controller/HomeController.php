7d5cb74f8e6752e8a487a24881869c69d0d46806
Rachmat Ardico Perdana <rahmatardiko99@gmail.com>
2022-10-01 15:24:20 +0700

    Model

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -6,6 +6,11 @@ class HomeController
 {
     function index():void
     {
+        $model = [
+            "title"     => "Belajar PHP MVC",
+            "content"   => "Selamat belajar PHP MVC dari Programmer Zaman Now"
+        ];
+
         echo "HomeController.index()";
     }
 
@@ -22,5 +27,22 @@ class HomeController
     function about():void
     {
         echo "Student : Rachmat Ardico Perdana";
+    }
+
+    function login():void
+    {
+        $request = [
+            "username"  => $_POST['username'],
+            "password"  => $_POST['password']
+        ];
+
+        $user = [
+
+        ];
+
+        $response = [
+            "message"     => "Login sukses"
+        ];
+        // kirimkan response ke view
     }
 }
