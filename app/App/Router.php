639ae8a59a2aa9ce6487f8be80421136e83f8520
Rachmat Ardico Perdana <rahmatardiko99@gmail.com>
2022-10-01 13:51:08 +0700

    Kompleks Router

---
+++ app/App/Router.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Matt\PhpMvc\App;
+
+class Router
+{
+    private static array $routes = [];
+
+    public static function add(string $method, string $path, string $controller, string $function):void
+    {
+        self::$routes[] = [
+            'method'        => $method,
+            'path'          => $path,
+            'controller'    => $controller,
+            'function'      => $function
+        ];
+    }
+
+    public static function run():void
+    {
+        $path = '/';
+        if (isset($_SERVER['PATH_INFO'])) {
+            $path = $_SERVER['PATH_INFO'];
+        }
+
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        foreach (self::$routes as $route) {
+            if ($route['path'] == $path && $route['method'] == $method) {
+                echo "Controller : " . $route['controller'] . ", Function : " . $route['function'];
+                return;
+            }
+        }
+
+        http_response_code(404);
+        echo "CONTROLLER NOT FOUND";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php 
 
-$path = "/index";
+require_once __DIR__ . '/../vendor/autoload.php';
 
-if (isset($_SERVER['PATH_INFO'])) {
-    $path = $_SERVER['PATH_INFO'];
-}
+use Matt\PhpMvc\App\Router;
 
-require __DIR__ . '/../app/View' . $path . ".php";
+Router::add('GET', '/', 'HomeController', 'index');
+Router::add('GET', '/login', 'UserController', 'login');
+Router::add('GET', '/register', 'UserController', 'register');
+
+Router::run();
